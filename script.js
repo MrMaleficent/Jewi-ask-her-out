f2ed15a3ee41b5baf430ade1724b3d222eb65a3a
Jeffwin Daniel <118381518+MrMaleficent@users.noreply.github.com>
2025-01-25 00:07:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function selectOption(option) {
 
 // Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var colors = ['#301934', '#000000', '#D8B9FF', '#953553', '#00FFFF', '#FF0000', '#800020'];
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
